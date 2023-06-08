786173566873faa2bcc61ef178c0a0aeb05d6907
mr-jackpot <adam.heeps@live.co.uk>
2023-06-08 19:05:47 +0100

    Calculating NIC

--- src/business/calculateTakeHomePay.js
+++ src/business/calculateTakeHomePay.js
@@ -1,15 +1,21 @@
 import countryCodes from '../data/countryCodes.json';
 
+const calculateTakeHome = async (country, salary) => {
+    const json = await jsonSearcher('name', country.toUpperCase());
+    const tax = calculateTax(json.tax, salary);
+    const nic = calculateNic(json.nic, salary);
+    return (tax - nic).toFixed(2);
+}
+
 const jsonSearcher = async (key, value) => {
     for (let i = 0; i < countryCodes.length; i++) {
         if (countryCodes[i][key] === value) {
-            return countryCodes[i].tax;
+            return countryCodes[i];
         }
     }
     return null;
 };
-const calculate = async (country, salary) => {
-    const bands = await jsonSearcher('name', country.toUpperCase());
+const calculateTax = (bands, salary) => {
     let result = 0;
     let money = salary;
     let skip = false;
@@ -24,7 +30,26 @@ const calculate = async (country, salary) => {
             skip = true;
         }
     });
-    return result.toFixed(2);
+    return result;
 };
 
-export default calculate;
+const calculateNic = (bands, salary) => {
+    let result = 0;
+    let money = salary;
+    let skip = false;
+    bands.forEach((band) => {
+        if (skip || band.rate === 1) {
+            return;
+        }
+        if (band.end < money) {
+            result += (band.end - band.start) * band.rate;
+        } else {
+            result += (money - band.start) * band.rate;
+            if (result < 0) result = 0;
+            skip = true;
+        }
+    });
+    return result;
+}
+
+export default calculateTakeHome;
