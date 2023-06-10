31cd845fe5b5e8494786d5d4e65b1db012eec466
mr-jackpot <adam.heeps@live.co.uk>
2023-06-10 09:52:36 +0100

    Integrated pension calculatiosn

--- src/views/Main.js
+++ src/views/Main.js
@@ -4,24 +4,32 @@ import "./Main.css";
 import TakeHomePay from "../components/take-home-pay/TakeHomePay";
 import BaseSalary from "../components/base-salary/BaseSalary";
 import calculateTakeHome from "../business/calculateTakeHomePay";
+import PensionContribution from "../components/pension-contribution/PensionContribution";
 
 const Main = () => {
     const [salary, setSalary] = React.useState(0);
+    const [pension, setPension] = React.useState(0);
     const [takeHomePay, setTakeHomePay] = React.useState(0);
     const sendSalary = (salary) => {
         setSalary(salary);
-    }
+    };
+
+    const sendPension = (pension) => {
+        setPension(pension);
+    };
+
     useEffect(() => {
-        calculateTakeHome('SCOTLAND', salary).then((result) => {
+        calculateTakeHome('SCOTLAND', salary, pension).then((result) => {
             setTakeHomePay(result);
         });
-    }, [salary]);
+    }, [salary, pension]);
 
     return (
         <div className={"stack-container"}>
             <Stack spacing={2}>
                 <TakeHomePay pay={takeHomePay}/>
                 <BaseSalary sendSalary={sendSalary}/>
+                <PensionContribution sendPension={sendPension}/>
             </Stack>
         </div>
     );
