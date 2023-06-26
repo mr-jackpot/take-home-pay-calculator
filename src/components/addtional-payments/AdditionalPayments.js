7031f7c243fd6bf8098a16be143fc5b2b5314747
mr-jackpot <adam.heeps@live.co.uk>
2023-06-26 21:36:57 +0100

    Finished additional payments js

--- src/components/addtional-payments/AdditionalPayments.js
+++ src/components/addtional-payments/AdditionalPayments.js
@@ -3,6 +3,19 @@ import {Grid, OutlinedInput, InputAdornment} from "@mui/material";
 import './AdditionalPayments.css';
 
 const AdditionalPayments = (props) => {
+
+    const [bonus, setBonus] = React.useState(0);
+    const [overtime, setOvertime] = React.useState(0);
+    const bonusValueChange = (e) => {
+        setBonus(e);
+        props.sendBonus(e);
+    }
+
+    const overtimeValueChange = (e) => {
+        setOvertime(e);
+        props.sendOvertime(e);
+    }
+
     return (
         <Grid container
               direction="row"
@@ -17,6 +30,8 @@ const AdditionalPayments = (props) => {
                     startAdornment={<InputAdornment position="start">£</InputAdornment>}
                     sx={{width: 300}}
                     type={"number"}
+                    value={bonus}
+                    onChange={(e) => bonusValueChange(e.target.value)}
                 />
             </Grid>
             <Grid item textAlign={"center"}>
@@ -25,6 +40,8 @@ const AdditionalPayments = (props) => {
                     startAdornment={<InputAdornment position="start">£</InputAdornment>}
                     sx={{width: 300}}
                     type={"number"}
+                    value={overtime}
+                    onChange={(e) => overtimeValueChange(e.target.value)}
                 />
             </Grid>
         </Grid>
